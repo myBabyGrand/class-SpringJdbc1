1a15a2cb503b00286cde5db1f0ee459be989a57c
mybabygrand <amuro_ray@kakao.com>
2022-05-12 22:52:59 +0900

    Section1. JDBC 이해  - H2 DB setting  - connection test

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/springdb";
+    public static final String USERNAME = "SA";
+    public static final String PASSWORD = "";
+}
